b580acba3e1c762f8aa096da0332cc5c0ad2ea0c
Paula Burghelea <paulaburghelea.github@gmail.com>
2024-03-30 13:43:57 +0000

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana, Poppins } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ["400" , "700"], subsets: ['latin'] });
+export const poppins = Poppins({ weight: ["300" , "700"], subsets: ['latin'] });
